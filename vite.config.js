74e43242d5300d284909254c66a14950cdc456a3
baraka26 <barakaalinaitwe@gmail.com>
2025-08-04 15:07:54 -0400

    Committed

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,10 @@
-// vite.config.js
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
-    open: true, // Auto-opens browser on dev start
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true, // Useful for debugging production builds
-  },
+    host: true,
+    port: 3000, // Replit expects the app to run on port 3000
+  }
 });
