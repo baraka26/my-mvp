da05e9945f83c7cca54078718a079ec9abac7ef3
baraka26 <barakaalinaitwe@gmail.com>
2025-08-04 15:21:34 -0400

    Committed

--- main.jsx
+++ main.jsx
@@ -1,17 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-const rootElement = document.getElementById('root')
-if (!rootElement) {
-  throw new Error("Root element not found. Ensure your HTML has <div id='root'></div>.")
-  }
-
-  createRoot(rootElement).render(
-    <StrictMode>
-        <App />
-          </StrictMode>
-          )
-          
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
