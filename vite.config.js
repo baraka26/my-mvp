35f9054aace457e092d851dd08fa8415141244d0
baraka26 <barakaalinaitwe@gmail.com>
2025-08-06 18:28:14 -0400

    Commit Code

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
+    host: '0.0.0.0'
     port: 3000,
-    allowedHosts: ['.replit.dev'] // This line allows Replit’s generated hostnames
-  }
+  strictPort: true
+  },
 });
